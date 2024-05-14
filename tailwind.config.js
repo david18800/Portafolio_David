64430a0b1d3f791aed080f905fb9179b93151700
david18800 <hippie181800@gmail.com>
2024-05-13 19:43:47 -0500

    feature
    install library

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pl1_1: '#B21C77',
+        pl1_2: '#922EA8',
+        pl1_3: '#348151',
+        pl1_4: '#4E4E6C',
+        pl1_5: '#16152E',
+        pl1_6: '#7850BA',
+        pl1_7: '#5B3879',
+      }
+
+    },
   },
   plugins: [],
 }
